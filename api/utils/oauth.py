a0cb6d37cbb93ccafb66dbff60b93799efd776c0
Waptech <abdulfataiyakub07@gmail.com>
2025-01-01 23:58:55 +0100

    added oauth config for github authorization

--- api/utils/oauth.py
+++ api/utils/oauth.py
@@ -23,6 +23,16 @@ oauth.register(
     client_kwargs={"scope": "openid email profile"},
 )
 
+# Register GitHub OAuth
+oauth.register(
+    name="github",
+    client_id=config.GITHUB_CLIENT_ID,
+    client_secret=config.GITHUB_CLIENT_SECRET,
+    access_token_url="https://github.com/login/oauth/access_token",
+    authorize_url="https://github.com/login/oauth/authorize",
+    api_base_url="https://api.github.com/",
+    client_kwargs={"scope": "user:email"},
+)
 
 async def get_google_authorization_url(request: Request) -> str:
     redirect_uri = config.GOOGLE_REDIRECT_URI
@@ -38,3 +48,24 @@ async def get_google_user_info(request: Request):
         logging.info(f"User Info Retrieved: {user_info}\nToken-:{token}")
         return user_info
     
+
+async def get_github_authorization_url(request: Request) -> str:
+    redirect_uri = config.GITHUB_REDIRECT_URI
+    
+    authorization_url = await oauth.github.authorize_redirect(request, redirect_uri)
+    return str(authorization_url)
+
+async def get_github_user_info(request: Request):
+    token = await oauth.github.authorize_access_token(request)
+    logging.info(f"Token: {token}")
+    user_resp = await oauth.github.get('user', token=token)
+    user_info = user_resp.json()
+    logging.info(f"User info: {user_info}")
+    
+    # Fetch primary email
+    email_resp = await oauth.github.get('user/emails', token=token)
+    emails = email_resp.json()
+    primary_emails = [email['email'] for email in emails if email.get('primary') and email.get('verified')]
+    user_info['email'] = primary_emails[0] if primary_emails else None
+    
+    return user_info
