2d5bdca1b42f47054d9e347d173c17aee6a26a6a
Waptech <abdulfataiyakub07@gmail.com>
2025-01-01 23:51:40 +0100

    user utils file with password and jwt token configurations

---
+++ api/utils/user.py
@@ -0,0 +1,109 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from jose import JWTError, jwt
+from datetime import datetime, timedelta
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+
+from api.v1.models.user import User
+from api.utils.settings import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+from api.db.session import get_db
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+security = HTTPBearer()
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def create_access_token(user_id: str) -> str:
+    expires = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    data = {"user_id": user_id, "exp": expires, "type": "access"}
+    encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def create_refresh_token(user_id: str) -> str:
+    expires = datetime.now() + timedelta(days=30)
+    data = {"user_id": user_id, "exp": expires, "type": "refresh"}
+    encoded_jwt = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception) -> str:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("user_id")
+        
+        if user_id is None or payload.get("type") != "access":
+            raise credentials_exception
+        
+        return user_id
+    
+    except JWTError:
+        raise credentials_exception
+
+
+def verify_refresh_token(token: str, credentials_exception) -> str:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("user_id")
+        token_type = payload.get("type")
+        
+        if user_id is None or token_type != "refresh":
+            raise credentials_exception
+        
+        return user_id
+    
+    except JWTError:
+        raise credentials_exception
+
+
+def refresh_access_token(current_refresh_token: str):
+    credentials_exception = HTTPException(
+        status_code=401, detail="Refresh token expired"
+    )
+
+    user_id = verify_refresh_token(current_refresh_token, credentials_exception)
+
+    access_token = create_access_token(user_id=user_id)
+    refresh_token = create_refresh_token(user_id=user_id)
+
+    return access_token, refresh_token
+
+
+# def get_current_user(token: str, db: Session = Depends(get_db)) -> User:
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+#     user_id = verify_access_token(token, credentials_exception)
+#     user = db.query(User).filter(User.id == user_id).first()
+#     if user is None:
+#         raise credentials_exception
+#     return user
+
+# Get current user from the token
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    db: Session = Depends(get_db)
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    token = credentials.credentials
+    user_id = verify_access_token(token, credentials_exception)
+    
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+    return user
